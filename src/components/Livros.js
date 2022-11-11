0402787f134e909ec7e595c43f8a6f0b5bf0afcd
Deyvid Kasteh <99744584+Deyvid-Kasteh@users.noreply.github.com>
2022-11-11 18:53:04 -0300

    primeiro fetch

---
+++ src/components/Livros.js
@@ -0,0 +1,16 @@
+import styles from "./Livros.module.css";
+
+function Livros( livros ) {
+
+        console.log( livros )
+
+  return (
+    <div className={`${styles.livros}`}>
+          {/* <p>`teste ${resultadoLivros[0]}`</p>       */}
+          <p>oi</p>
+      
+    </div>
+  )
+}
+
+export default Livros;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,55 @@
 import styles from "./Main.module.css";
+import { BsSearch } from "react-icons/bs";
+import {useState} from 'react'
+import Livros from "./Livros";
 
 function Main() {
+
+  
+  
+
+  const [pesquisa, setPesquisa] = useState('Biblioteca')
+
+  
+  let [resultadosLivros, setResultadosLivros] = useState();
+
+
+  function BuscarLivros(pesquisa) {
+    fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=${pesquisa}&maxResults=40`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((resp) => resp.json())
+      // .then((data) => console.log(data['items']))
+      .then((data) => setResultadosLivros(data["items"]))
+      .catch((err) => console.log(err));
+  }
+
+  console.log(resultadosLivros)
+
+
+
   return (
     <div className={`${styles.Main_container}`}>
-      <h1 className={`${styles.title}`}>Biblioteca.</h1>
-      <input className={`${styles.input_busca}`} type="text" name="input_busca" placeholder="Digite o nome do livro"/>
+      <h1 className={`${styles.title}`}>{pesquisa || "Biblioteca"}.</h1>
+      <div className={`${styles.form}`}>
+        <input
+          className={`${styles.input_busca}`}
+          type="text"
+          name="input_busca"
+          placeholder="Digite o nome do livro"
+          onChange={(e) => setPesquisa(e.target.value)}
+        />
+        <button className={`${styles.btn_busca}`} onClick={BuscarLivros}>
+          <BsSearch />
+        </button>
+      </div>
+      <Livros livros={resultadosLivros} />
     </div>
   );
 }
