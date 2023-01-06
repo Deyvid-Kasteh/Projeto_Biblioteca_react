b3024e7e0ca7a32a9b8c12be55129058cd1e9b35
Deyvid Kasteh <kasteh21@gmail.com>
2023-01-06 01:08:19 -0300

    change BaseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
 
-// export const api = axios.create({
-//     baseURL: 'https://projeto-biblioteca-react-server.vercel.app/src/server.js'
-// });
-
-
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL:
+    "https://projeto-biblioteca-react-server-3-1.vercel.app",
 });
+
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:5000",
+// });
 
 
 export const createSession = async (email, password) => {
