555c0381c6bd91f33da290c88c63aa611b5da85d
Deyvid Kasteh <kasteh21@gmail.com>
2023-01-27 17:06:27 -0300

    melhorias signUp page 5

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,15 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { api } from "../services/api";
 import zxcvbn from "zxcvbn";
-
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Navbar from "./Navbar";
 import styles from "./SignUp.module.css";
 import FooterBack from "./FooterBack";
 
 // ICONS ----------------------------------------------------------------
-
-import { BsFillCheckCircleFill, BsCheckCircle } from "react-icons/bs";
 
 import { FaCheck, FaCheckCircle } from "react-icons/fa";
 
@@ -23,6 +21,9 @@ function SignUp() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [change, setChange] = useState(false);
+
+
   const passwordStrengthScore = zxcvbn(password).score;
 
   const insuficiente = "😭";
@@ -37,37 +38,125 @@ function SignUp() {
   const valido = <FaCheckCircle />;
 
   const regExpEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-  let formFilled = false;
 
-  if (
-    name.length > 0 &&
-    email.length > 0 &&
-    password.length > 0 &&
-    confirmPassword > 0
-  ) {
-    formFilled = true;
-  } else {
-    formFilled = false;
-  }
+  const validName = useRef(false);
+  const validEmail = useRef(false);
+  const validPassword = useRef(false);
+  const validConfirmPassword = useRef(false);
 
-  // console.log(regExpEmail.test(email));
-  console.log(formFilled)
+  const formFilled = useRef(false);
+
+  const notify = () => toast.success("Senhas iguais");
+
+  const validNameFunction = (name) => {
+    if (name.length >= 3) {
+      validName.current = true;
+      // console.log(`${"Nome SIM"}`);
+      return <h2 className={styles.icons}>{valido}</h2>;
+    } else {
+      validName.current = false;
+      // console.log(`${"Nome NÃO"}`);
+      return "";
+    }
+  };
+
+  const validEmailFunction = (email) => {
+    if (regExpEmail.test(email)) {
+      validEmail.current = true;
+      // console.log(`${"Email SIM"}`);
+      return <h2 className={styles.icons}>{valido}</h2>;
+    } else {
+      validEmail.current = false;
+      // console.log(`${"Email NÃO"}`);
+      return <h2>{teste}</h2>;
+    }
+  };
+
+  const validPasswordFunction = (password) => {
+    if (password.length > 0) {
+      validPassword.current = true;
+      // console.log(`${"Password SIM"}`);
+      return passwordStrengthSwitch(passwordStrengthScore);
+    } else {
+      validPassword.current = false;
+      // console.log(`${"Password NÃO"}`);
+      return <h2>{falha}</h2>;
+    }
+  };
+
+  const validConfirmPasswordFunction = (confirmPassword, password) => {
+    if (confirmPassword === password) {
+      validConfirmPassword.current = true;
+      // console.log(`${"ConfirmPassword SIM"}`);
+      return <h2 className={styles.icons}>{valido}</h2>;
+    } else {
+      validConfirmPassword.current = false;
+      // console.log(`${"ConfirmPassword NÃO"}`);
+      return <h2>{falha}</h2>;
+    }
+  };
 
   const passwordStrengthSwitch = (passwordStrengthScore) => {
     switch (passwordStrengthScore) {
       case 0:
+        validPassword.current = false;
+
         return <h2>{insuficiente}</h2>;
       case 1:
+        validPassword.current = false;
+
         return <h2>{fraco}</h2>;
       case 2:
+        validPassword.current = true;
+        console.log(`${"TROCOU MEDIO"}`);
+        console.log(`${"TROCOU MEDIO"}`);
+        console.log(`${"TROCOU MEDIO"}`);
+        console.log(`${"TROCOU MEDIO"}`);
+
         return <h2>{medio}</h2>;
       case 3:
+        validPassword.current = true;
+        console.log(`${"TROCOU BOM"}`);
+        console.log(`${"TROCOU BOM"}`);
+        console.log(`${"TROCOU BOM"}`);
+        console.log(`${"TROCOU BOM"}`);
+        console.log(`${"TROCOU BOM"}`);
+
         return <h2>{bom}</h2>;
       case 4:
+        validPassword.current = true;
+        console.log(`${"TROCOU FORTE"}`);
+        console.log(`${"TROCOU FORTE"}`);
+        console.log(validPassword.current);
+        console.log(`${"TROCOU FORTE"}`);
+        console.log(`${"TROCOU FORTE"}`);
+
         return <h2>{forte}</h2>;
     }
   };
 
+  const formFilledFunction = () => {
+    if (
+      validName.current &&
+      validEmail.current &&
+      validPassword.current &&
+      validConfirmPassword.current
+    ) {
+      formFilled.current = true;
+      notify();
+      setChange(prev=> !prev)
+      console.log(`${"formFilled SIM"}`);
+    } else {
+      formFilled.current = false;
+      console.log(`${"formFilled NÃO"}`);
+            setChange((prev) => !prev);
+
+      console.log(`${validName.current} + validName`);
+      console.log(`${validEmail.current} + validEmail`);
+      console.log(`${validPassword.current} + validPassword`);
+      console.log(`${validConfirmPassword.current} + validConfirmPassword`);
+    }
+  }
   const handleSignUp = async (e) => {
     e.preventDefault();
     if (email === "" || password === "") {
@@ -112,14 +201,22 @@ function SignUp() {
                 value={name}
                 placeholder="Apelido..."
                 autoComplete="off"
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                onKeyUp={() => {
+                  formFilledFunction();
+                }}
               />
-              <div className={`${styles.feedback}`}>
+              {/* <div className={`${styles.feedback}`}>
                 {name.length >= 3 ? (
                   <h2 className={styles.icons}>{valido}</h2>
                 ) : (
                   ""
                 )}
+              </div> */}
+              <div className={`${styles.feedback}`}>
+                {validNameFunction(name)}
               </div>
             </div>
             <div className={styles.formGroup}>
@@ -131,16 +228,24 @@ function SignUp() {
                 value={email}
                 placeholder="Email..."
                 autoComplete="off"
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                onKeyUp={() => {
+                  formFilledFunction();
+                }}
               />
 
               {email ? (
+                // <div className={`${styles.feedback}`}>
+                //   {regExpEmail.test(email) ? (
+                //     <h2 className={styles.icons}>{valido}</h2>
+                //   ) : (
+                //     <h2>{teste}</h2>
+                //   )}
+                // </div>
                 <div className={`${styles.feedback}`}>
-                  {regExpEmail.test(email) ? (
-                    <h2 className={styles.icons}>{valido}</h2>
-                  ) : (
-                    <h2>{teste}</h2>
-                  )}
+                  {validEmailFunction(email)}
                 </div>
               ) : (
                 ""
@@ -155,15 +260,23 @@ function SignUp() {
                 value={password}
                 placeholder="Senha..."
                 autoComplete="off"
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                onKeyUp={() => {
+                  formFilledFunction();
+                }}
               />
               {password ? (
+                // <div className={`${styles.feedback}`}>
+                //   {password.length === 0 ? (
+                //     <h2>{falha}</h2>
+                //   ) : (
+                //     passwordStrengthSwitch(passwordStrengthScore)
+                //   )}
+                // </div>
                 <div className={`${styles.feedback}`}>
-                  {password.length === 0 ? (
-                    <h2>{falha}</h2>
-                  ) : (
-                    passwordStrengthSwitch(passwordStrengthScore)
-                  )}
+                  {validPasswordFunction(password)}
                 </div>
               ) : (
                 ""
@@ -179,15 +292,16 @@ function SignUp() {
                 value={confirmPassword}
                 placeholder="Confirme a senha..."
                 autoComplete="off"
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+                onKeyUp={() => {
+                  formFilledFunction();
+                }}
               />
               {confirmPassword ? (
                 <div className={`${styles.feedback}`}>
-                  {confirmPassword === password ? (
-                    <h2 className={styles.icons}>{valido}</h2>
-                  ) : (
-                    <h2>{falha}</h2>
-                  )}
+                  {validConfirmPasswordFunction(confirmPassword, password)}
                 </div>
               ) : (
                 ""
@@ -197,7 +311,9 @@ function SignUp() {
             <div
               // className={styles.formGroup_btn}
               className={`${
-                formFilled ? styles.formGroup_btnFilled : styles.formGroup_btn
+                formFilled.current
+                  ? styles.formGroup_btnFilled
+                  : styles.formGroup_btn
               }`}
             >
               <button
@@ -216,6 +332,18 @@ function SignUp() {
             </div>
           </form>
         </div>
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </div>
       <FooterBack />
     </div>
